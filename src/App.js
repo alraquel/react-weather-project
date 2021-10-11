750e1804b629cdaebcddbdabb091fa556478c5d0
alraquel <87284570+alraquel@users.noreply.github.com>
2021-10-11 17:17:30 +0100

    added link to github

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ export default function App() {
         <input type="Search" placeholder="enter a city" />
         <button type="Submit">Search</button>
       </form>
+      <p className="coded">
+        <a href="https://github.com/alraquel"> Coded</a>
+        by Raquel
+      </p>
     </div>
   );
 }
