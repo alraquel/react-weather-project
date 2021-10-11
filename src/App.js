87e169538d222513df186c071d0e5c3d3766efca
alraquel <87284570+alraquel@users.noreply.github.com>
2021-10-11 17:12:14 +0100

    added search engine html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="SearchEngine">
+      <form>
+        <input type="Search" placeholder="enter a city" />
+        <button type="Submit">Search</button>
+      </form>
     </div>
   );
 }
-
-export default App;
